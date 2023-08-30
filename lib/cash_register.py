649228ab2d2f9730e735ca898ad02448f7a13d77
Slonetech <kengeregordon@gmail.com>
2023-08-30 13:16:54 +0300

    completed assignments

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  
+  def __init__(self, ones, twos, fives, tens, twenties):
+    self._ones = ones
+    self._twos = twos
+    self._fives = fives
+    self._tens = tens
+    self._twenties = twenties
+ 
+  def __str__(self):
+    return "CashRegister = {0} {1} {2} {3} {4}".format(self._ones, self._twos, self._fives, self._tens, self._twenties)
+  
+  def get_total(self):
+    return self._ones + self._twos * 2 + self._fives * 5 + self._tens * 10 + self._twenties * 20
+     
+  
+  def apply_discount (self, discount):
+    if discount > 0:
+      return self.get_total() * (1 - discount/100)
+    else:
+      return self.get_total()
+    
+  def void_last_transaction(self):
+    self._total -= self._last_transaction
+    self._last_transaction = 0
